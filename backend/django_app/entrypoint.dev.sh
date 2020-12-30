ae7f491d113150d292ae0c603976a91670985639
Benjamin Kurmann <tech@kurmail.ch>
2020-12-30 10:43:35 +0100

    docker django dev

--- backend/django_app/entrypoint.dev.sh
+++ backend/django_app/entrypoint.dev.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# exit when error
+set -e
+
 if [ "$DATABASE" = "postgis" ]
 then
     echo "Waiting for postgis..."
@@ -18,5 +21,9 @@ python manage.py migrate --no-input
 # collect all static
 echo "collect static files..."
 python manage.py collectstatic --noinput
+
+echo "from users.models import CustomUser;
+CustomUser.objects.filter(email='$DJANGO_ADMIN_EMAIL').delete();
+CustomUser.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
 
 exec "$@"
