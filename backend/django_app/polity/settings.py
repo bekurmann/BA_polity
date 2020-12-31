07c8ef9714d67cbee6f5dd38bda05d1acc0e87de
Benjamin Kurmann <tech@kurmail.ch>
2020-12-31 10:52:17 +0100

    docker compose prod

--- backend/django_app/polity/settings.py
+++ backend/django_app/polity/settings.py
@@ -185,11 +185,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'uploads'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 
 # ***********************************************************************************************
 # CUSTOM USER MODEL
