945f65c3fd6dd0d02adee7dc4b91caedf27e7dd9
Benjamin Kurmann <tech@kurmail.ch>
2021-01-25 11:04:32 +0100

    added github link

--- frontend/nuxt_app/middleware/auth-user.js
+++ frontend/nuxt_app/middleware/auth-user.js
@@ -1,4 +1,5 @@
 export default async function ({ app, redirect }) {
+    // https://github.com/nuxt-community/auth-module/issues/478
     // the following look directly for the cookie created by nuxtjs/auth
     // instead of using $auth.loggedIn
     const user = await app.$cookies.get('auth._token.local')
