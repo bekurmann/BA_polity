5238548b680f1648347cd2816776566a5f4b2782
Benjamin Kurmann <tech@kurmail.ch>
2021-02-01 09:29:29 +0100

    added memberships to politican serializer

--- backend/django_app/core/api/serializers/fraction.py
+++ backend/django_app/core/api/serializers/fraction.py
@@ -3,14 +3,11 @@ from rest_framework import serializers
 # import models
 from core.models import Fraction
 
-#from core.api.serializers import ParlamentSerializer
-
 class FractionSerializer(serializers.ModelSerializer):
     """
     model serializer for fraction
     """
-    #parlament = ParlamentSerializer(read_only=True, many=True)
 
     class Meta:
         model = Fraction
-        exclude = ['created_at', 'parlament',]
+        fields = '__all__'
--- backend/django_app/core/api/serializers/politican.py
+++ backend/django_app/core/api/serializers/politican.py
@@ -5,14 +5,16 @@ from core.models import Politican
 
 # import additional serializers 
 from locations.api.serializers import PLZSerializer
-from core.api.serializers import FractionSerializer, ParlamentSerializer
+from core.api.serializers import FractionSerializer, CommissionSerializer
 
 
 class PoliticanSerializer(serializers.ModelSerializer):
     city = PLZSerializer(read_only=True)
-    #fractions = FractionSerializer(read_only=True)
+    fractions = FractionSerializer(read_only=True, many=True)
+    commissions = CommissionSerializer(read_only=True, many=True)
+    #parties = 
     #parlaments = ParlamentSerializer(read_only=True)
  
     class Meta:
         model = Politican
-        exclude = ['created_at', 'updated_at', 'location_query', ]
+        exclude = ['location_query', ]
