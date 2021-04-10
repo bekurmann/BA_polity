e82dec44a627477d0ae9992d440908ee0d2f7c07
Benjamin Kurmann <tech@kurmail.ch>
2021-04-10 14:13:10 +0200

    added some information to api response from parlament detail

--- backend/django_app/core/api/serializers/parlament.py
+++ backend/django_app/core/api/serializers/parlament.py
@@ -1,10 +1,13 @@
 from rest_framework import serializers
 
 # import models
-from core.models import Parlament
+from core.models import Parlament, Affair, AffairDebate
 
 # import additional serializers 
 from locations.api.serializers import CantonNestedSerializer, PLZSerializer
+
+# import for prefetching
+from django.db.models import Prefetch
 
 # *****************************************************************************************
 # Parlament
@@ -27,16 +30,31 @@ class ParlamentDetailSerializer(serializers.ModelSerializer):
 
     number_of_registered_members = serializers.SerializerMethodField()
     number_of_fractions = serializers.SerializerMethodField()
+    number_of_affairs = serializers.SerializerMethodField()
+    number_of_debate_statements = serializers.SerializerMethodField()
+    number_of_sessions = serializers.SerializerMethodField()
 
     class Meta:
         model = Parlament
         exclude = ['created_at', 'updated_at', 'location_query']
 
-    def get_number_of_registered_members(self, members):
-        return members.parlament_memberships.filter(
+    def get_number_of_registered_members(self, parlament):
+        return parlament.parlament_memberships.filter(
                         membership_type="PARLA", 
                         membership_function="MEMBE"
                         ).count()
 
-    def get_number_of_fractions(self, fractions):
-        return fractions.fractions.count()
+    def get_number_of_fractions(self, parlament):
+        return parlament.fractions.count()
+
+    def get_number_of_affairs(self, parlament):
+        return parlament.affairs_parlaments.count()
+
+    def get_number_of_debate_statements(self, parlament):
+        debates = AffairDebate.objects.filter(affair__parlament=parlament)
+        return debates.count()
+
+    def get_number_of_sessions(self, parlament):
+        return parlament.parlaments.count()
+
+    
