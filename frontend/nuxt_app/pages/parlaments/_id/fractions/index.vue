547e11fa041b8f4280f640ebb9186eaa6ae30281
Benjamin Kurmann <tech@kurmail.ch>
2021-04-16 09:27:02 +0200

    temporary view for fractions

---
+++ frontend/nuxt_app/pages/parlaments/_id/fractions/index.vue
@@ -0,0 +1,19 @@
+<template>
+
+        <v-card class="mt-3">
+            <v-card-title primary-title>
+                Fractions - coming soon
+            </v-card-title>
+        </v-card>
+
+</template>
+<script>
+
+export default {
+    components: {
+        
+    },
+    middleware: ['auth'],
+    layout: 'ParlamentDetail',
+}
+</script>
