47fe33c5c245b16a2bdc1341720f0c84b48402db
Benjamin Kurmann <tech@kurmail.ch>
2021-04-16 16:34:35 +0200

    adjusted cli pipeline

--- backend/django_app/entrypoint.prod.sh
+++ backend/django_app/entrypoint.prod.sh
@@ -14,8 +14,18 @@ then
     echo "postgis started"
 fi
 
+# migrate (only dev)
+echo "migrate..."
+python manage.py migrate --run-syncdb --no-input
+
 # collect all static
 echo "collect static files..."
 python manage.py collectstatic --noinput
+
+# standard superuser
+echo "creating super user ..."
+echo "from users.models import CustomUser;
+CustomUser.objects.filter(email='$DJANGO_ADMIN_EMAIL').delete();
+CustomUser.objects.create_superuser('$DJANGO_ADMIN_USER', '$DJANGO_ADMIN_EMAIL', '$DJANGO_ADMIN_PASSWORD')" | python manage.py shell
 
 exec "$@"
