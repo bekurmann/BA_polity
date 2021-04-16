e425d685fe4c2132964e7c7f771b5b388a6cf58f
Benjamin Kurmann <tech@kurmail.ch>
2021-04-16 13:22:37 +0200

    added support site

---
+++ frontend/nuxt_app/pages/support/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-card class="primary" dark>
+                  <!-- start main -->
+                  <v-container>
+
+                    <v-row>
+                      <v-col cols="12" xs="12" sm="12" md="6" lg="6" xl="6">
+                        <p class="text-h3 mt-15">This Feature is not yet available.</p><br>
+                        <p class="text-h5">
+                            If you got any experience with Django DRF or Nuxt, checkt out our repository.<br>
+                            Or help us in another way! Contact us!
+                        </p>
+                        <p><v-btn outlined x-large to="">Find out how</v-btn></p>
+                      </v-col>
+                    </v-row>
+
+                  </v-container>
+
+                </v-card>
+            </v-col>
+        </v-row>
+        
+    </v-container>
+</template>
+<script>
+export default {
+
+}
+</script>
