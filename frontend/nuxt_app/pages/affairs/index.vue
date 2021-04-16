df555983f6c285c44f513c8b2b2d9313a521d790
Benjamin Kurmann <tech@kurmail.ch>
2021-04-16 09:24:10 +0200

    temporary view little nicer

--- frontend/nuxt_app/pages/affairs/index.vue
+++ frontend/nuxt_app/pages/affairs/index.vue
@@ -1,10 +1,24 @@
 <template>
-    <v-card>
-        List view?
-    </v-card>
+
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-card>
+                    <v-card-title primary-title>
+                        Affairs
+                    </v-card-title>
+                    <v-card-subtitle>Check out already registered affairs. </v-card-subtitle>
+                    <v-card-text>
+                        List view Affairs (all parlaments?) -> newest first?
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+        
+    </v-container>
 </template>
 <script>
 export default {
-
+    middleware: ['auth'],
 }
 </script>
