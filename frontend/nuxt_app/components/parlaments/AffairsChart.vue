16eb9f7a5b3788a55ff167dfa2ea559fa4f75c05
Benjamin Kurmann <tech@kurmail.ch>
2021-04-23 17:10:30 +0200

    working on analysis overview

---
+++ frontend/nuxt_app/components/parlaments/AffairsChart.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-card>
+        <v-card-title>Chart</v-card-title>
+        <v-card-text>
+            <LineChart :data="lineChartData" :options="lineChartOptions" :height="150"></LineChart>
+        </v-card-text>
+    </v-card>
+</template>
+<script>
+import LineChart from '~/components/LineChart.js'
+
+const chartColors = {
+  red: 'rgb(255, 99, 132)',
+  orange: 'rgb(255, 159, 64)',
+  yellow: 'rgb(255, 205, 86)',
+  green: 'rgb(0, 153, 76)',
+  blue: 'rgb(54, 162, 235)',
+};
+
+export default {
+    data() {
+        return {
+            lineChartData: {
+                labels: this.affairs.map(affairs => affairs.date_received),
+                datasets: [
+                    {
+                        label: 'Affairs',
+                        data: Object.keys(this.affairs)                
+                    }
+                ]
+            },
+            lineChartOptions: {
+                responsive: true,
+                legend: {
+                    position: "top",
+                },
+                title: {
+                    display: true,
+                    text: 'Affairs OW 2018-2022',
+                    position: 'top',
+                    fontSize: 18,
+                },
+            }
+        }
+    },
+    props: {
+        affairs: {
+            type: Object
+        },
+    },
+    components: {
+        LineChart,
+    }
+}
+</script>
--- frontend/nuxt_app/pages/parlaments/_id/analysis/index.vue
+++ frontend/nuxt_app/pages/parlaments/_id/analysis/index.vue
@@ -1,17 +1,31 @@
 <template>
 
-        <v-card class="mt-3">
-            <v-card-title primary-title>
-                Analysis - coming soon
-            </v-card-title>
-        </v-card>
+    <v-container ma-0 mt-3 pa-0>
+        <v-row>
+            <v-col cols="12" xs="12" sm="12" md="6" lg="6" xl="6">
+                <AffairsChart :affairs="affairs"></AffairsChart>
+            </v-col>
+
+            <v-col cols="12" xs="12" sm="12" md="6" lg="6" xl="6">
+                <!-- <FractionsChart :fractions="fractions"></FractionsChart> -->
+            </v-col>
+        </v-row>
+    </v-container>
 
 </template>
 <script>
+import AffairsChart from '~/components/parlaments/AffairsChart.vue'
+
 
 export default {
+    async asyncData({params, $axios}) {
+        const affairs = await $axios.$get(`/parlaments/${params.id}/affairs/`)
+        return { 
+            affairs: affairs,
+        }
+    },
     components: {
-        
+        AffairsChart,
     },
     middleware: ['auth'],
     layout: 'ParlamentDetail',
